1718963786176c2c941bc61f3a78894f8fa23379
Jose Mendoza <jmendozarocha96@outlook.com>
2026-09-08 09:41:59 -0500

    Actualizacion del archivo

--- app/ramas.py
+++ app/ramas.py
@@ -6,3 +6,48 @@
 #         <a href="#">GitHub</a>
 #     </nav>
 # </header>
+----------------------------------------
+
+## Flujo de trabajo**
+
+1. git pull
+2. npm run dev
+3. Desarrollar
+4. git status
+5. git add .
+6. git commit -m "mensaje"
+7. git push
+
+----------------------------------------
+
+## Actualizar proyecto**
+
+Traer cambios desde GitHub
+
+git pull origin main
+
+----------------------------------------
+
+## Backend FastAPI**
+
+Activar entorno virtual
+
+cd backend
+.\venv\Scripts\activate
+
+Iniciar FastAPI
+
+uvicorn app.main:app --reload
+
+----------------------------------------
+
+## Frontend React**
+
+Volver a la raíz del proyecto
+
+cd ..
+
+Iniciar React / Vite
+
+npm run dev
+----------------------------------------
